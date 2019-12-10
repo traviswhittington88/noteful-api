73624cfb77e79cc7823f3123aec1c3a92013bef1
traviswhittington88 <traviswhittington@ymail.com>
2019-12-09 18:57:12 -0600

    modified DB_URL env var

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
+  "connectionString": process.env.DATABASE_URL,
 }
 
